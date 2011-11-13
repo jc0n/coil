768d1743c1b383a7f4dc5af1b498b0faba327302
John O'Connor <tehjcon@gmail.com>
2011-11-13 00:07:51 -0500

    Add private struct api

---
+++ coil/struct-private.h
@@ -0,0 +1,33 @@
+/*
+ * Author: John O'Connor
+ * Copyright 2011
+ */
+
+#ifndef _COIL_STRUCT_PRIVATE
+#define _COIL_STRUCT_PRIVATE
+
+gboolean
+finalize_prototype(CoilObject *self, gpointer unused);
+
+
+CoilObject *
+_coil_create_containers(CoilObject *self, CoilPath *path,
+        gboolean prototype, gboolean has_lookup, GError **error);
+
+gboolean
+coil_struct_mark_deleted_path(CoilObject *self, CoilPath *path,
+        gboolean force, GError **error);
+
+gboolean
+coil_struct_add_dependency(CoilObject *self,
+        CoilObject *object, GError **error);
+
+gboolean
+coil_struct_extend_path(CoilObject *self, CoilPath *path,
+        CoilObject *context, GError **error);
+
+gboolean
+coil_struct_extend_paths(CoilObject *self, GList *list,
+        CoilObject *context, GError **error);
+
+#endif
